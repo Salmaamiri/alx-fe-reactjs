8dcb82c1c422aa36193daca80962e9692e0d1d97
Sigurd <synchasgard@gmail.com>
2025-07-23 00:04:59 +0100

    recipeStore.js

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -4,14 +4,15 @@ import { create } from 'zustand';
 const useRecipeStore = create((set) => ({
   recipes: [],
   searchTerm: '',
-  setSearchTerm: (term) => set({ searchTerm: term }),
-  filteredRecipes: [],
-  filterRecipes: () =>
+  setSearchTerm: (term) => {
+    set({ searchTerm: term });
     set((state) => ({
       filteredRecipes: state.recipes.filter((recipe) =>
         recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
       ),
-    })),
+    }));
+  },
+  filteredRecipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   deleteRecipe: (recipeId) =>
