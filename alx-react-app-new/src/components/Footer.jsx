6592e0edff96e3d9d89058305b09d3b07ed7e778
Oussama Lasfar <synchasgard@gmail.com>
2025-07-22 19:46:50 +0100

    state mana

--- alx-react-app-new/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#222', color: 'white', textAlign: 'center', padding: '1rem' }}>
-      <p>© 2023 City Lovers</p>
-    </footer>
-  );
-}
-export default Footer;
--- alx-react-app-new/src/components/MainContent.jsx
+++
@@ -1,8 +0,0 @@
-function MainContent() {
-  return (
-    <main style={{ backgroundColor: '#f9f9f9', padding: '1rem', textAlign: 'center' }}>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
-    </main>
-  );
-}
-export default MainContent;
