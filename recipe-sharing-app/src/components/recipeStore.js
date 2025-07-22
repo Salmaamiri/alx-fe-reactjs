ae2d213d614e7216aa178d494680b0799300e1b1
Sigurd <synchasgard@gmail.com>
2025-07-22 23:40:24 +0100

    recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,7 +4,18 @@ import { create } from 'zustand';
 const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  deleteRecipe: (recipeId) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+    })),
+  updateRecipe: (recipeId, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === recipeId ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
   setRecipes: (recipes) => set({ recipes }),
 }));
 
 export default useRecipeStore;
+
