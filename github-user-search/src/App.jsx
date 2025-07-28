f6cebcd1127a17ed6dbafbec0a3e79b3f7d3a5ab
salma <salma0amiri@example.com>
2025-07-28 23:55:52 +0100

    task0

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <p>Start searching for GitHub users!</p>
+    </div>
+  );
+}
+
+export default App;
